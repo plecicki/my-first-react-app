1f87360af736ede0e22639b7eb1e0eaad2b7cc5e
Piotr Lecicki <p.lecicki@gmail.com>
2025-03-15 18:51:03 +0100

    Add container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Hero from './components/Hero/Hero'
 import SearchForm from './components/SearchForm/SearchForm'
 import List from './components/List/List'
+import Container from "./components/Container/Container";
 
 const App = () => {
   return (
-    <div>
-      <Hero />
-      <SearchForm />
-      <List />
-    </div>
+    <Container >
+      <div>
+        <Hero />
+        <SearchForm />
+        <List />
+      </div>
+    </Container>
   );
 };
 
